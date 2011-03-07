132c48446d609006d3b961ee69b4e56036cc2ee0
Zach <mulcrone@gmail.com>
2011-03-07 00:19:01 -0600

    starting chap 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ SampleApp::Application.routes.draw do
 
   get "pages/contact"
   
-  get "pages/about/"
+  get "pages/about"
+  
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
